595922eb6a4d8ac0d65730ea65fd9c40d3da58be
krzy-sok <01178595@pw.edu.pl>
2025-10-25 14:51:21 +0200

    experimental table in configuration.py

--- openwisp_monitoring/monitoring/configuration.py
+++ openwisp_monitoring/monitoring/configuration.py
@@ -766,6 +766,12 @@ DEFAULT_METRICS = {
             },
         },
     },
+    "test":{
+        "label": _("test probe"),
+        "name": "test probe",
+        "key": "testprobe",
+        "field_name": "test_field",
+    }
 }
 
 DEFAULT_CHARTS = {}
