6105e2f7574053139ef6accb46fad77b9562748c
krzy-sok <01178595@pw.edu.pl>
2026-02-18 13:14:26 +0100

    fix: typo + typing

--- openwisp_monitoring/device/writer.py
+++ openwisp_monitoring/device/writer.py
@@ -301,7 +301,7 @@ class DeviceDataWriter(object):
                 metric,
                 probe["rtt"],
                 current,
-                time = datetime.fromtimestamp(probes["device_timestamp"]),
+                time = datetime.fromtimestamp(int(probe["device_timestamp"])),
                 extra_values={"mac": probe["mac"], "flood_flag": probe["flood_flag"], "interface": probe["interface"]},
             )
             if created:
