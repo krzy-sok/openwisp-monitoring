9d0c28aa206a4eeda7a3d9de81451126f15ffbda
krzy-sok <01178595@pw.edu.pl>
2026-03-10 19:23:12 +0100

    fix: import path

--- openwisp_monitoring/device/writer.py
+++ openwisp_monitoring/device/writer.py
@@ -9,7 +9,7 @@ from swapper import load_model
 
 from .. import settings as monitoring_settings
 from ..monitoring.configuration import ACCESS_TECHNOLOGIES
-from antisniff_classifier_iterface import get_prediction
+from .antisniff_classifier_iterface import get_prediction
 
 Chart = load_model("monitoring", "Chart")
 Metric = load_model("monitoring", "Metric")
@@ -306,7 +306,7 @@ class DeviceDataWriter(object):
                     "individual_probes": ", ".join(str(rtt) for rtt in rtts)
                 },
             )
-            self._write_antisniff_prediction(host['ip'], avg, median, primary_key, content_type, current, time)
+            # self._write_antisniff_prediction(host['ip'], avg, median, primary_key, content_type, current, time)
             if created:
                 self._create_resources_chart(metric, resource="probes")
 
@@ -314,7 +314,7 @@ class DeviceDataWriter(object):
         metric, created = Metric._get_or_create(
             object_id=primary_key,
             content_type_id=content_type.id,
-            configuration="sniffer_prediction",
+            configuration="sniffer_proba",
             name = f"{"ip"} predictions",
             main_tags={"ip": Metric._makekey('ip')},
         )
---
+++ openwisp_monitoring/monitoring/tests/test_model_prodict.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+from django.test import TestCase
+from django.core.cache import cache
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
+
+from swapper import load_model
+
+from openwisp_monitoring.device.tests import TestDeviceMonitoringMixin
+from openwisp_monitoring.device.tests.test_admin import DeviceData
+from openwisp_monitoring.device.antisniff_classifier_iterface import get_prediction
+
+from . import TestMonitoringMixin
+
+start_time = timezone.now()
+ten_minutes_ago = start_time - timedelta(minutes=10)
+Metric = load_model("monitoring", "Metric")
+AlertSettings = load_model("monitoring", "AlertSettings")
+Notification = load_model("openwisp_notifications", "Notification")
+
+class TestPredictModel(TestMonitoringMixin, TestCase):
+    def tearDown(self):
+        cache.clear()
+        super().tearDown()
+
+    def test_custom_get_or_create(self):
+        m, created = Metric._get_or_create(name="sniffer_proba", configuration="sniffer_proba")
+        self.assertTrue(created)
+        m2, created = Metric._get_or_create(name="sniffer_proba", configuration="sniffer_proba")
+        self.assertEqual(m.id, m2.id)
+        self.assertFalse(created)
+
+class TestPredictDeviceData(TestDeviceMonitoringMixin, TestCase):
+    # def test_write_predict_data():
+    #     pass
+    def _create_device_data(self, **kwargs):
+        d = self._create_device(**kwargs)
+        return DeviceData(pk=d.pk)
+
+    def test_get_prediction_inaccessible(self):
+        dd = self._create_device_data()
+        res = get_prediction(dd.pk, "10.0.0.1", 0.5, 0.5)
+        assert(res == -1.0)
