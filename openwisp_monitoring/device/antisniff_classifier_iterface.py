dc1c4b0c84149b955bb09178db4cecbe211f913c
krzy-sok <01178595@pw.edu.pl>
2026-04-04 12:29:21 +0200

    fix: changed antisniff request method

--- openwisp_monitoring/device/antisniff_classifier_iterface.py
+++ openwisp_monitoring/device/antisniff_classifier_iterface.py
@@ -2,7 +2,7 @@ import requests
 
 def get_prediction(device_id, host_ip, avg, median, flood_flag):
     try:
-        res = requests.request("get", "http://antisniff:8001/predict", data={"rtt_avg": avg, "rtt_median": median, "flood_flag": flood_flag})
+        res = requests.post("http://antisniff:8001/predict", json={"rtt_avg": avg, "rtt_median": median, "flood_flag": flood_flag})
         return float(res.text)
     except Exception:
         return -1.0
